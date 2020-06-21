6190272e290b0a94685ad0391f3c91c73802f72c
lukman <lukmanhakim174@gmail.com>
2020-06-21 19:13:26 +0700

    complete swagger

--- src/main/java/com/cpworks/CpWorksApplication.java
+++ src/main/java/com/cpworks/CpWorksApplication.java
@@ -2,8 +2,12 @@ package com.cpworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan("com.cpworks")
 public class CpWorksApplication {
 
 	public static void main(String[] args) {
