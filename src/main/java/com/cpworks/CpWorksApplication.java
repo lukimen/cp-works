7ae3d8616452c8f369c6b0ce3bb2cb3fe9f303aa
lukman <lukmanhakim174@gmail.com>
2020-06-21 16:58:51 +0700

    init project

---
+++ src/main/java/com/cpworks/CpWorksApplication.java
@@ -0,0 +1,13 @@
+package com.cpworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpworks/CpWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
