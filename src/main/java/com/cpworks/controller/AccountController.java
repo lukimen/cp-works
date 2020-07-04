887c1282ec543a1e9f9a0b4493d2592834aff6bc
lukman <lukmanhakim174@gmail.com>
2020-07-04 15:36:28 +0700

    Update AccountController.java

--- src/main/java/com/cpworks/controller/AccountController.java
+++ src/main/java/com/cpworks/controller/AccountController.java
@@ -6,7 +6,6 @@ import com.cpworks.model.ResponseCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,15 +21,15 @@ public class AccountController {
 
 	@ApiOperation(value = "register", notes = "register account")
 	@GetMapping("/register")
-	public BaseResponse<String> register(
+	public BaseResponse<Boolean> register(
 			@RequestParam(required = true) String email,
 			@RequestParam(required = true) String password) {
 
 		accountDaoService.register(email, password);
 
-		return BaseResponse.<String>builder()
+		return BaseResponse.<Boolean>builder()
 				.code(ResponseCode.SUCCESS.getCode())
-				.data("Register Sukses")
+				.data(true)
 				.build();
 	}
 
